d006b8aa9d38a1699922dd4b634175cbafbc56a2
Luca <luca.brusadelli@laplateforme.io>
2022-10-26 09:27:16 +0200

    adding main file

---
+++ main.py
@@ -0,0 +1,14 @@
+from functions import *
+
+anthill = 1
+
+anthills_names = {1 : '../Fourmilieres/fourmiliere_un.txt', \
+                    2 : '../Fourmilieres/fourmiliere_deux.txt', \
+                    3 : '../Fourmilieres/fourmiliere_trois.txt', \
+                    4 : '../Fourmilieres/fourmiliere_quatre.txt', \
+                    5 : '../Fourmilieres/fourmiliere_cinq.txt' \
+                       }
+
+file_name = anthills_names[anthill]
+
+param = import_data(file_name)
