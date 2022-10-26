ccc6b18ae227450a843257eb0010aa4a5bcbee45
roux-michon-stehane <stephane.roux-michon@laplateforme.io>
2022-10-26 17:17:54 +0200

    ajout de la fonction graph et mise a jour de main

--- graph.py
+++ graph.py
@@ -1,18 +1,23 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-chemin = nx.Graph()
 
-chemin.add_nodes_from([
-    (1,{"salle":"vestibule"}),
-    (2,{"salle":"s1"}),
-    (3,{"salle":"s2"}),
-    (4,{"salle":"dortoir"})
-])
+def graph(node_list,edge_list):
+    chemin = nx.Graph()
+    
+    chemin.add_nodes_from(node_list)
 
-chemin.add_edges_from([(1,2),(2,3),(3,4)])
+    chemin.add_edges_from(edge_list)
+    """
+    chemin.add_nodes_from([
+        (1,{"salle":"vestibule"}),
+        (2,{"salle":"s1"}),
+        (3,{"salle":"s2"}),
+        (4,{"salle":"dortoir"})
+    ])
 
-nx.draw(chemin, with_labels=True, font_weight='bold')
+    chemin.add_edges_from([(1,2),(2,3),(3,4)])
+    """
 
-print(chemin)
-plt.show() 
+    nx.draw(chemin, with_labels=True, font_weight='bold')
+    plt.show() 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from functions import *
+from graph import *
 
-anthill = 1
+anthill = 4
 
 anthills_names = {1 : '../Fourmilieres/fourmiliere_un.txt', \
                     2 : '../Fourmilieres/fourmiliere_deux.txt', \
@@ -12,3 +13,10 @@ anthills_names = {1 : '../Fourmilieres/fourmiliere_un.txt', \
 file_name = anthills_names[anthill]
 
 param = import_data(file_name)
+
+node_list = [*range(4)]
+
+edge_list = name_to_index(param[2],param[4])
+print(param[2],edge_list)
+
+graph( node_list,edge_list)
